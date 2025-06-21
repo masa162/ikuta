9fc383e541f02f41307677e814b3d2640f1a35e7
masa162 <belong2jazz@gmail.com>
2025-06-21 16:30:50 +0900

    fix: remove clchair data and add proper koijutu layout

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "clchair | くらしと健康の相談室";
-export const SITE_DESCRIPTION = "clchair.net は、健康と暮らしをつなぐ個人事業『クリーン チェア』の公式サイトです。";
+export const SITE_TITLE = "古今医術研究所";
+export const SITE_DESCRIPTION = "医療学術論文東西比較";
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
