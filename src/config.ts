ab44bf57980e5b8b3e51bda0945e70c0a05f7bf8
masa162 <belong2jazz@gmail.com>
2025-06-22 09:28:07 +0900

    Initial commit: ikuta project

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "浅夢紫月";
-export const SITE_DESCRIPTION = "医療学術論文東西比較";
+export const SITE_TITLE = "Ikuta_Green";
+export const SITE_DESCRIPTION = "Ikuta_Green";
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
